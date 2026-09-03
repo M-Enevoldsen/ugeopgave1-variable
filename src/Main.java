c7b429314e24f41a6ff2d43d1fe3347f06100833
M-Enevoldsen <mads0907.me@gmail.com>
2026-09-03 11:01:11 +0200

    anden commit

---
+++ src/Main.java
@@ -0,0 +1,184 @@
+import java.util.Arrays;
+
+public class Main {
+
+    void main(){
+      //Opgave 1: shopping cart
+        /*
+       double item1 = 25.50;
+       double item2 = 15.00;
+       double item3 = 30.00;
+       double subtotalItems = item1 + item2 + item3;
+        double momsProcent = 25.0;
+        double subtotalWithMoms = subtotalItems + subtotalItems * momsProcent / 100;
+        System.out.println(subtotalItems);
+        System.out.println(subtotalWithMoms);
+         */
+
+    //Opgave 2 split restuarant bill
+        /*
+        double food = 450.0;
+        double drinks = 120;
+        double tips = 50;
+        double people = 4;
+        double bill = food + drinks + tips;
+        double pricePerPerson = bill / people;
+        System.out.println(pricePerPerson);
+
+         */
+
+    //Opgave 3 discount calculation
+        /*
+    double originalPrice = 300.0;
+    double discountPercent = 20.0;
+    double discountedPrice = originalPrice - originalPrice *discountPercent / 100;
+    double momspercent = 25.0;
+    double finalPrice = discountedPrice + discountedPrice * momspercent / 100;
+        System.out.println(originalPrice);
+        System.out.println(discountedPrice);
+        System.out.println(finalPrice);
+
+         */
+
+    //Opgave 4 party planning
+        /*
+        int guest = 15;
+        int cupcakes = 40;
+        int cupcakesEach = cupcakes / guest;
+        int cupcakesLeft = cupcakes % guest;
+        System.out.println(cupcakesEach);
+        System.out.println(cupcakesLeft);
+
+         */
+
+    //Opgave 5 build greeting message
+        /*
+        StringBuilder sb = new StringBuilder();
+        String name = "Anna";
+        int age = 25;
+        sb.append("hello, ");
+        sb.append(name);
+        sb.append("!, ");
+        sb.append("you are ");
+        sb.append(age);
+        sb.append(" years old.");
+        System.out.println(sb);
+
+         */
+
+    //Opgave 6 Using +=
+        /*
+        int score = 100;
+        System.out.println(score);
+        score += 50;
+        System.out.println(score);
+        score += 25;
+        System.out.println(score);
+
+         */
+
+    //Opgave 7 Using -=
+        /*
+        int health = 100;
+        health -= 15;
+        System.out.println(health);
+        health -= 20;
+        System.out.println(health);
+
+         */
+
+    //Opgave 8 Using *=
+        /*
+        double price = 50.0;
+        System.out.println(price);
+        price *= 2;
+        System.out.println(price);
+
+         */
+
+    //Opgave 9 increment og decrement med 1
+        /*
+    int count = 10;
+        System.out.println(count);
+        count ++;
+        System.out.println(count);
+        count --;
+        System.out.println(count);
+
+         */
+
+    //opgave 10 all compound operators
+    int number = 10;
+    number += 5;
+        System.out.println(number);
+    number -= 3;
+        System.out.println(number);
+    number ++;
+        System.out.println(number);
+    number --;
+        System.out.println(number);
+
+    //Opgave 11 E-commerce product
+        /*
+    String productName = "laptop";
+    double productPrice = 7999.99;
+    boolean inStock = true;
+    int stockCount = 15;
+    double discountPercent = 10.0;
+    String category = "Electronics";'
+
+         */
+
+    //Opgave 12 Karaktersystem
+        /*
+    String studentName = "Anna";
+    int testScore = 85;
+    int assignmentScore = 92;
+    double finalGrade = 88.5;
+    boolean coursePassed = true;
+
+         */
+
+    //Opgave 13 bookingsystem
+        /*
+    String guestName = "Peter Hansen";
+    int roomNumber = 204;
+    int nightsStaying = 3;
+    double costPerNight = 899.00;
+    boolean breakfestIncluded = true-,
+    double totalPrice = 2697.00;
+
+         */
+
+    //Opgave 14 Fitness tracker
+        /*
+        int stepCount = 8547;
+        int stepGoal = 10000;
+        double distanceWalked = 6.2;
+        int caloriesBurnt = 420;
+        boolean goalReached = false;
+
+         */
+
+    //Opgave 15 bibliotekssystem
+        /*
+    String bookTitle;
+    String author;
+    String isbn;
+    int pageCount;
+    boolean isAvailable;
+    String returnDate;
+    int lateFee;
+    boolean bestseller;
+    double bookRating;
+
+         */
+
+
+
+
+
+
+
+    }
+}
